bcc5384c9912575657ffc54abf4805e5dc44b9e1
Mattia Lau <mattia.lau.0824@gmail.com>
2024-07-19 20:01:58 +0800

    chore: commitlint

---
+++ .commitlintrc.ts
@@ -0,0 +1,42 @@
+import type { UserConfig } from "@commitlint/types";
+
+const commitlintConfig: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "subject-case": [
+      2,
+      "always",
+      [
+        "sentence-case",
+        "start-case",
+        "pascal-case",
+        "upper-case",
+        "lower-case",
+      ],
+    ],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+    "scope-enum": [
+      1,
+      "always",
+      ["bun", "node", "common", "core", "graphql", "apollo"],
+    ],
+  },
+};
+
+export default commitlintConfig;
