07f2c6512ae09a67b22ce4746a3f983aa640aa53
Mattia Lau <mattia.lau.0824@gmail.com>
2024-08-04 20:34:09 +0800

    release: 0.0.10

--- example/packages/http/bun.ts
+++ example/packages/http/bun.ts
@@ -5,7 +5,6 @@ import { createApplication } from "@nexiojs/core";
 import { IMicroservice } from "@nexiojs/microservice";
 import { CORSInterceptor } from "./src/interceptors/cors.interceptor.ts";
 import { JwtInterceptor } from "./src/interceptors/jwt.interceptor.ts";
-import { Logger } from "@nexiojs/common";
 
 const main = async () => {
   const app = await createApplication<IMicroservice>({
@@ -15,9 +14,6 @@ const main = async () => {
     port: 3000,
   });
 
-  const logger = new Logger();
-  logger.info({ Hello: "World" });
-  logger.info({ foo: "bar" }, "baz %s", "qux");
   // await app.connectMicroservices([]);
 };
 
